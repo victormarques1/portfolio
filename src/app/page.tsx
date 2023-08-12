237ddd3390416bf8223f3288af0bc1f0f8395499
Victor Marques <marques.vct@hotmail.com>
2023-08-12 00:24:09 -0300

    fix: general responsivity + principal color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,40 +3,40 @@ import { useState } from "react";
 
 import Image from "next/image";
 import Foto from "./../assets/image/profile2.png";
-import Smile from "./../assets/image/smile.svg";
-import Email from "./../assets/image/email.svg";
-import Instagram from "./../assets/image/instagram.svg";
 import Phone from "./../assets/image/phone.svg";
-import Typescript from "./../assets/image/typescript.svg";
-import Node from "./../assets/image/node.svg";
-import Js from "./../assets/image/javascript.svg";
-import Tailwind from "./../assets/image/tailwind.svg";
 import React from "./../assets/image/react.svg";
-import Next from "./../assets/image/next.svg";
 import Micday from "./../assets/projects/login.png";
 import Link from "next/link";
 
-import { HiMenu } from "react-icons/hi";
-import { MdClose } from "react-icons/md";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { MdClose, MdOutlineMailOutline, MdOutlinePhone } from "react-icons/md";
+import { BiLogoLinkedin, BiLogoGithub } from 'react-icons/bi'
+import { FaNode } from 'react-icons/fa'
+import { SiJavascript, SiTypescript, SiReact, SiNextdotjs, SiTailwindcss } from 'react-icons/si'
 
 export default function Home() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <div className="max-w-5xl mx-auto px-4">
-        <nav className="flex items-center justify-between pt-8">
+      <div className="max-w-5xl mx-auto px-4 w-full">
+        <nav className="flex items-center justify-between pt-8 ">
           <div>
             <h2 className="font-bold text-2xl p-2 md:p-0">Portfólio</h2>
           </div>
 
-          <button className="md:hidden mx-2" onClick={()=>setIsMenuOpen(!isMenuOpen)}>
-            { isMenuOpen ? <MdClose size={26} /> : <HiMenu size={26} />}
+          <button
+            className="md:hidden mx-2 text-zinc-400 hover:text-zinc-300"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            {isMenuOpen ? <MdClose size={28} /> : <HiOutlineMenuAlt3 size={28} />}
           </button>
 
-          <ul className={`md:flex md:text-lg md:z-auto md:static absolute py-4 md:py-0 md:pl-0  
+          <ul
+            className={`md:flex md:text-lg md:z-auto md:static absolute py-4 md:py-0 md:pl-0  
           transition-all ease-in p-2 md:p-0
-          ${isMenuOpen ? "top-20" : "top-[-490px]"}`}>
+          ${isMenuOpen ? "top-20" : "top-[-490px]"}`}
+          >
             <li>
               <Link href="#about">
                 <h3 className="md:mx-4 md:my-0 hover:text-zinc-300 duration 500 ">
@@ -53,7 +53,7 @@ export default function Home() {
             </li>
             <li>
               <Link href="#skills">
-              <h3 className="md:mx-4 mt-3 md:my-0 hover:text-zinc-300 duration 500">
+                <h3 className="md:mx-4 mt-3 md:my-0 hover:text-zinc-300 duration 500">
                   Minhas skills
                 </h3>
               </Link>
@@ -61,20 +61,24 @@ export default function Home() {
           </ul>
         </nav>
 
-        <div className={`flex justify-between items-center md:mt-20 ${isMenuOpen ? "mt-48" : "mt-16"}`}>
+        <div
+          className={`flex md:flex-row flex-col md:justify-between justify-center items-center md:mt-20 ${
+            isMenuOpen ? "mt-48" : "mt-16"
+          }`}
+        >
           <section>
-            <h2 className="text-4xl font-bold">
+            <h2 className="md:text-4xl text-2xl font-bold">
               Olá, eu sou o <br></br> Victor Marques :)
             </h2>
-            <p className="py-4 text-zinc-500 text-lg font-medium mt-2 mb-4">
+            <p className="py-4 text-zinc-500 md:text-lg font-medium mt-2 mb-4">
               Desenvolvedor Full Stack
             </p>
-            <div className="font-medium text-lg ">
-              <button className="mr-4 text-neutral-900 bg-green-500 hover:font-semibold py-3 px-8">
+            <div className="flex md:flex-row flex-col font-medium md:text-lg ">
+              <button className="mr-4 text-neutral-900 bg-blueshirt hover:font-semibold py-3 px-8 mb-2 md:mb-0">
                 Download CV
               </button>
               <Link href="mailto:victorsmarqs@gmail.com">
-                <button className="border border-zinc-700 hover:font-semibold py-3 px-6">
+                <button className="border border-zinc-700 hover:font-semibold py-3 px-6 mb-14 md:mb-0">
                   Entrar em contato
                 </button>
               </Link>
@@ -89,12 +93,12 @@ export default function Home() {
           />
         </div>
 
-        <div className="flex flex-col items-center justify-center text-center mt-28">
+        <div className="flex flex-col items-center justify-center text-center mt-14 md:mt-28">
           <section className="flex flex-col items-center justify-center max-w-2xl">
-            <h2 id="about" className="font-bold text-3xl mb-6">
+            <h2 id="about" className="font-bold md:text-3xl text-2xl mb-6">
               Sobre mim
             </h2>
-            <span className="text-lg text-zinc-500">
+            <span className="md:text-lg text-zinc-500">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -104,39 +108,38 @@ export default function Home() {
             </span>
           </section>
 
-          <section className="flex flex-row mt-16">
-            <div className="flex flex-col items-center mr-12">
+          <section className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-14">
+            <div className="flex flex-col items-center justify-center">
               <div className="border border-transparent rounded-full p-3 bg-zinc-800 mb-4">
-                <Image src={Smile} width={32} height={32} alt="Smile icon" />
-              </div>
-              <strong>Meu nome</strong>
-              <p className="text-zinc-500">Victor Marques</p>
-            </div>
-
-            <div className="flex flex-col items-center justify-center mr-12">
-              <div className="border border-transparent rounded-full p-3 bg-zinc-800 mb-4">
-                <Image src={Email} width={32} height={32} alt="Email icon" />
+              <MdOutlineMailOutline size={28} className="text-blueshirt"/>
               </div>
               <strong>E-mail</strong>
               <p className="text-zinc-500">victorsmarqs@gmail.com</p>
             </div>
 
-            <div className="flex flex-col items-center justify-center mr-12">
+            <div className="flex flex-col items-center justify-center">
               <div className="border border-transparent rounded-full p-3 bg-zinc-800 mb-4">
-                <Image
-                  src={Instagram}
-                  width={32}
-                  height={32}
-                  alt="Instagram icon"
-                />
+                <BiLogoLinkedin size={28} className="text-blueshirt"/>
               </div>
-              <strong>Instagram</strong>
-              <p className="text-zinc-500">---</p>
+              <strong>LinkedIn</strong>
+              <Link href="https://www.linkedin.com/" target="_blank" className="text-zinc-500 hover:text-blue-300">
+              /victormarqs1
+              </Link>
             </div>
 
             <div className="flex flex-col items-center justify-center">
               <div className="border border-transparent rounded-full p-3 bg-zinc-800 mb-4">
-                <Image src={Phone} width={32} height={32} alt="Phone icon" />
+                <BiLogoGithub size={30} className="text-blueshirt"/>
+              </div>
+              <strong>GitHub</strong>
+              <Link href="https://github.com/victormarques1" target="_blank" className="text-zinc-500 hover:text-blue-300">
+              /victormarques1
+              </Link>
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
+              <div className="border border-transparent rounded-full p-3 bg-zinc-800 mb-4">
+                <MdOutlinePhone size={28} className="text-blueshirt"/>
               </div>
               <strong>Telefone</strong>
               <p className="text-zinc-500">(55) 99916-6533</p>
@@ -147,11 +150,11 @@ export default function Home() {
         <div>
           <h2
             id="projects"
-            className="flex justify-center font-bold text-3xl mt-28 mb-8"
+            className="flex justify-center font-bold md:text-3xl text-2xl md:mt-28 mt-14 mb-8 md:mb-10"
           >
             Projetos
           </h2>
-          <section className="grid gap-x-12 gap-y-8 grid-cols-3 justify-center text-center">
+          <section className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-8 justify-center text-center">
             <div className="border border-zinc-700 p-6 bg-zinc-800">
               <div className="border border-zinc-700 bg-red-50 mb-6">
                 <Image className="w-full" src={Micday} alt="Phone icon" />
@@ -217,41 +220,29 @@ export default function Home() {
         <div className="flex flex-col">
           <h2
             id="skills"
-            className="flex justify-center font-bold text-3xl mt-28 mb-8 text"
+            className="flex justify-center font-bold md:text-3xl text-2xl md:mt-28 mt-14 mb-8 md:mb-10"
           >
             Principais skills
           </h2>
-          <div className="grid grid-cols-6 gap-x-8 mb-12">
-            <div className="border border-zinc-700 p-4 bg-zinc-800 mb-4 flex justify-center">
-              <Image src={Js} width={70} height={70} alt="JavaScript icon" />
+          <div className="grid grid-cols-3 md:grid-cols-6 md:gap-8 gap-6 mb-8">
+            <div className="border border-zinc-700 p-6 items-center bg-zinc-800 mb-4 flex justify-center">
+              <SiJavascript size={68} className='text-blueshirt' />
             </div>
-            <div className="border border-zinc-700 bg-zinc-800 mb-4 flex justify-center">
-              <Image
-                src={Typescript}
-                width={70}
-                height={70}
-                alt="Typescript icon"
-              />
+            <div className="border border-zinc-700 p-6 items-center bg-zinc-800 mb-4 flex justify-center">
+              <SiTypescript size={68} className='text-blueshirt' />
             </div>
-            <div className="border border-zinc-700 p-4 bg-zinc-800 mb-4 flex justify-center">
-              <Image src={React} width={70} height={70} alt="React icon" />
+            <div className="border border-zinc-700 p-6 items-center bg-zinc-800 mb-4 flex justify-center">
+              <SiReact size={68} className='text-blueshirt' />
             </div>
-            <div className="border border-zinc-700 p-4 bg-zinc-800 mb-4 flex justify-center">
-              <Image src={Next} width={70} height={70} alt="Next icon" />
+            <div className="border border-zinc-700 p-6 items-center bg-zinc-800 mb-4 flex justify-center">
+              <SiNextdotjs size={68} className='text-blueshirt' />
             </div>
-
-            <div className="border border-zinc-700 p-4 bg-zinc-800 mb-4 flex justify-center">
-              <Image
-                src={Tailwind}
-                width={70}
-                height={70}
-                alt="Tailwind icon"
-              />
+            <div className="border border-zinc-700 p-6 items-center bg-zinc-800 mb-4 flex justify-center">
+              <SiTailwindcss size={68} className='text-blueshirt' />
+            </div><div className="border border-zinc-700 p-6 items-center bg-zinc-800 mb-4 flex justify-center">
+              <FaNode size={68} className='text-blueshirt' />
             </div>
-
-            <div className="border border-zinc-700 p-4 bg-zinc-800 mb-4 flex justify-center">
-              <Image src={Node} width={100} height={100} alt="Node icon" />
-            </div>
+          
           </div>
         </div>
       </div>
@@ -259,7 +250,7 @@ export default function Home() {
         <p className="text-lg ">
           Desenvolvido por{" "}
           <Link
-            className="text-green-500 font-bold"
+            className="text-blueshirt font-bold"
             href="https://github.com/victormarques1"
           >
             Victor Marques
